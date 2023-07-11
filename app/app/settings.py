ec13b8107067f9556b33e227df65fa2da357a4ef
Gavrić Sava <gavric.s.sava@gmail.com>
2023-07-11 22:34:45 +0200

    Add db service and initial docker-compose file

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        "NAME": config('MYSQL_DATABASE'),
+        "USER": config('MYSQL_USER'),
+        "PASSWORD": config('MYSQL_PASSWORD'),
+        "HOST": config('MYSQL_HOST'),
+        "PORT": config('MYSQL_PORT'),
     }
 }
 
