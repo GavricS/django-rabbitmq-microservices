aab369c83522bd65ca9ac697fb7a0f5ecc7d8e36
Gavrić Sava <gavric.s.sava@gmail.com>
2023-07-21 16:02:04 +0200

    Add adminer service

--- order/app/app/views.py
+++ order/app/app/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from .serializers import OrderSerializer
 from rest_framework import generics
 from .models import Order
+from decouple import config
 import pika
 
 QUEUE_ORDER_NAME = 'OrderQueue'
@@ -31,23 +32,23 @@ class OrderRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = OrderSerializer
 
 @api_view(['GET'])
-def order_checkout(request, id):# this should be refactored
+def order_checkout(request, pk):# this should be refactored
     try:
-        order = Order.objects.get(pk=id)
+        order = Order.objects.get(pk=pk)
     except Order.DoesNotExist:# TODO exception handling
         return Response(status=404)
     
-    serializer = OrderSerializer(order)
-    serialized_order_data = Response(serializer.data)
+    order_serializer = OrderSerializer(order)
+    serialized_order_data = Response(order_serializer.data)
 
-    order_queue_name = QUEUE_ORDER_NAME# extract into env var
+    order_queue_name = QUEUE_ORDER_NAME# TODO extract into env var
     order_exchange_name = EXCHANGE_ORDER_NAME
     financial_exchange_name = EXCHANGE_FINANCIAL_NAME
     request_processed = False
     res = None
 
     # Establish a connection to RabbitMQ
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))# TODO make sure there is no potential deadlock - inspect the BlockingConnection class for more info
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=config('RABBITMQ_HOST')))# TODO make sure there is no potential deadlock - inspect the BlockingConnection class for more info
     channel = connection.channel()
 
     # Declare exchanges and queues
@@ -60,31 +61,33 @@ def order_checkout(request, id):# this should be refactored
     message = str(serialized_order_data)
 
     # Send message to exchange
-    channel.basic_publish(exchange=order_exchange_name, routing_key='', body=message)
+    channel.basic_publish(exchange=order_exchange_name, routing_key='', body=message)# TODO what is 'routing_key'
 
     # Callback function for receiving messages
     def on_message(channel, method, properties, body):
+        # TODO maybe additional message confirmation logic needed?
         nonlocal res, request_processed
-        res = body
+        res = body# TODO body of 'bytes' type
         request_processed = True
 
     # Receive message
     channel.basic_consume(queue=order_queue_name, on_message_callback=on_message, auto_ack=True)
 
     # Wait for only 1 message with a timeout of 30 seconds
-    while request_processed != True:
-        try:
-            channel.connection.process_data_events(time_limit=30)
-        except KeyboardInterrupt:
-            break
-        except pika.exceptions.AMQPError as e:
+    # while request_processed != True:# TODO endless loop
+    try:
+        channel.connection.process_data_events(time_limit=5)
+    # except KeyboardInterrupt:# TODO is needed?
+        # break
+    except pika.exceptions.AMQPError as e:
             print(f"AMQP Error: {e}")
-            break
+            # break
 
     # Close connection and channel
     channel.close()
     connection.close()
 
     # Return result
-    return res
+    # return res
+    return Response(res, status=status.HTTP_200_OK)# TODO return type?
     # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
