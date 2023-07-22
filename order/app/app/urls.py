6715a7efaedcf053a882a82b75d094a932030d63
Gavrić Sava <gavric.s.sava@gmail.com>
2023-07-22 02:24:04 +0200

    Update order service endpoint paths

--- order/app/app/urls.py
+++ order/app/app/urls.py
@@ -5,8 +5,8 @@ from .views import OrderListCreateView, OrderRetrieveUpdateDestroyView, order_ch
 
 urlpatterns = [
     re_path(r"^admin/?", admin.site.urls),# TODO remove?
-    re_path(r"^api/orders/?$", OrderListCreateView.as_view(), name="order-list-create"),
-    re_path(r"^api/orders/(?P<pk>\d+)/?$", OrderRetrieveUpdateDestroyView.as_view(), name="order-retrieve-update-destroy"),
-    re_path(r"^api/orders/checkout/(?P<pk>\d+)/?$", order_checkout, name="order-checkout"),
+    re_path(r"^api/v1/orders/?$", OrderListCreateView.as_view(), name="order-list-create"),
+    re_path(r"^api/v1/orders/(?P<pk>\d+)/?$", OrderRetrieveUpdateDestroyView.as_view(), name="order-retrieve-update-destroy"),
+    re_path(r"^api/v1/orders/checkout/(?P<pk>\d+)/?$", order_checkout, name="order-checkout"),
     path("", default_urlconf)# TODO remove
 ]
